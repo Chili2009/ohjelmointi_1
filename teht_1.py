2a2f1934a9ebea5a309eef57141055cfb4286e2e
Mia <mia.fernandezrannikko@metropolia,fi>
2022-09-05 11:14:04 +0300

    uusi

---
+++ teht_1.py
@@ -0,0 +1,8 @@
+Kuha = float(input("Kuhan pituus senttimetreinä: "))
+
+if Kuha>=37:
+    print("Nosta kuha")
+if Kuha<=37:
+    print("Laske kuha vapaaksi")
+if Kuha<=37:
+    print(f"pyyntimitasta puuttuu {37-Kuha}cm ")
