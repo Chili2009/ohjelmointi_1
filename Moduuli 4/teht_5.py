51748c7a28e6caad734635ba9475455dd98da4ad
Mia <mia.fernandezrannikko@metropolia,fi>
2022-09-07 22:59:01 +0300

    Moduuli 4

--- Moduuli 4/teht_5.py
+++ Moduuli 4/teht_5.py
@@ -3,3 +3,19 @@
 #Tätä jatketaan kunnes kirjautumistiedot ovat oikein tai väärät tiedot on syötetty viisi kertaa.
 #Edellisessä tapauksessa tulostetaan Tervetuloa ja jälkimmäisessä Pääsy evätty.
 #(Oikea käyttäjätunnus on python ja salasana rules).
+username = "python"
+password = "rules"
+yritys = input(f"Syötä Käyttäjätunnus: ")
+yritys2 = input("Syötä Salasana: ")
+i = 1
+while i < 5:
+    if yritys == username and yritys2 == password:
+        print("Tervetuloa")
+        break
+    else:
+        print("Pääsy evätty")
+        i += 1
+        yritys = input(f"Syötä Käyttäjätunnus: ")
+        yritys2 = input("Syötä Salasana: ")
+if i == 5:
+    print("Pääsy evätty")
