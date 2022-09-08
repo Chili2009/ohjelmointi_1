d1b3107ee6526f891b78a8bb61240f9b9815c051
Mia <mia.fernandezrannikko@metropolia,fi>
2022-09-08 14:01:00 +0300

    Moduuli 5

--- Moduuli 5/teht_4.py
+++ Moduuli 5/teht_4.py
@@ -4,3 +4,13 @@
 # järjestyksessä kuin ne syötettiin. käytä for-toistorakennetta nimien kysymiseen
 # ja for/in toistorakennetta niiden läpikäymiseen.
 
+# Kaupungin nimet
+
+kaupungit = []
+
+for n in range(5):
+    kaupunki = input("Anna kapungin nimi")
+    kaupungit.append(kaupunki)
+    #print(kaupungit) välituloste jos epäilee ettei ohjelma toimi
+for n in kaupungit:
+    print(n)
